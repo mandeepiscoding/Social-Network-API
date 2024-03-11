0b5b1affb42a47ce84e5fca39f11f52448cec53c
Mandeep Singh <mandeepsingh@mandeeps-air.lan>
2024-03-11 02:51:02 -0700

    Added index.js and added models for User, Thought, and Reaction.

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual("friendCount").get(function () {
+  return this.friends.length;
+});
+userSchema.virtual("thoughtCount").get(function () {
+  return this.thoughts.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
